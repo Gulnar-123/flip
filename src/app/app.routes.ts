86a446ad554dca0cc2537ce4dfdb967ebe65ad80
Gulnar-123 <gulnar6816@gmail.com>
2025-02-04 00:00:45 +0530

    second commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-    {path:"",component:AppComponent}
+    {path:"home",component:HomeComponent},
+    {path:"", redirectTo:"home",pathMatch:"full"},
+    {path:"**", redirectTo:"home",pathMatch:"full"},
+
 ];
